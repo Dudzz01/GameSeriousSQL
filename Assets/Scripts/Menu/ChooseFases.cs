fe7098bc651f1bec549df39e39c5d225de3d4d4b
Eduardo Mury Assafin <101987520+Dudzz01@users.noreply.github.com>
2025-03-31 23:58:33 -0300

    feat sistema básico menu + fases
    busquei implementar o sistema simples de navegação de menu e escolha de fases

---
+++ Assets/Scripts/Menu/ChooseFases.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseFases : MonoBehaviour
+{
+    public void Fase1()
+    {
+
+        SceneManager.LoadScene("Fase 1");
+    }
+
+    public void Fase2()
+    {
+
+        SceneManager.LoadScene("Fase 2");
+    }
+
+    public void Fase3()
+    {
+
+        SceneManager.LoadScene("Fase 3");
+    }
+
+    public void Fase4()
+    {
+
+        SceneManager.LoadScene("Fase 4");
+    }
+
+    public void Fase5()
+    {
+
+        SceneManager.LoadScene("Fase 5");
+    }
+
+    public void Fase6()
+    {
+
+        SceneManager.LoadScene("Fase 6");
+    }
+
+    public void Fase7()
+    {
+
+        SceneManager.LoadScene("Fase 7");
+    }
+
+    public void Fase8()
+    {
+
+        SceneManager.LoadScene("Fase 8");
+    }
+
+    public void Fase9()
+    {
+
+        SceneManager.LoadScene("Fase 9");
+    }
+
+    public void Fase10()
+    {
+
+        SceneManager.LoadScene("Fase 10");
+    }
+
+
+
+
+}
