f01d958f2c1aa0542a865295e3b9ebc63e044381
Eduardo Mury Assafin <101987520+Dudzz01@users.noreply.github.com>
2025-04-21 14:44:38 -0300

    feat animacao player + sistema de dialogo

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,29 +1,60 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator))]
 public class Player : MonoBehaviour
 {
-    
+    [SerializeField] private float moveSpeed = 1.4f;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Vector2 movement;
+    private Vector2 lastDir;
+
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    
-    void Update()
+
+  
+void Update()
     {
-        movimentacaoPlayer();
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement = movement.normalized;
+
+        if (movement.sqrMagnitude > 0f)
+        {
+            lastDir = movement;
+            animator.SetFloat("MoveX", movement.x);
+            animator.SetFloat("MoveY", movement.y);
+            animator.speed = 0.6f;           
+        }
+        else
+        {
+            
+            animator.SetFloat("MoveX", lastDir.x);
+            animator.SetFloat("MoveY", lastDir.y);
+
+            
+            var stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+            
+            animator.Play(
+                stateInfo.fullPathHash,   
+                0,                        
+                1f                        
+            );
+
+            
+            animator.speed = 0f;
+        }
     }
-
-    void movimentacaoPlayer()
+    void FixedUpdate()
     {
-        
-
-        float speed_h = Input.GetAxis("Horizontal") * 1.4f;
-        float speed_v = Input.GetAxis("Vertical")* 1.4f;   
-
-        Vector2 movPlayer = new Vector2(speed_h, speed_v);
-        this.gameObject.GetComponent<Rigidbody2D>().velocity = movPlayer;
+        // Move o Rigidbody2D
+        rb.velocity = movement * moveSpeed;
     }
 }
